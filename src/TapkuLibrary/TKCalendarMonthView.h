c9c7b84249cf8f2d126d4998c7e5a9e4a52a133c
Tomasz Krasnyk <krt@coresystems.ch>
2013-08-02 15:53:26 +0200

    - Renamed old and new files

--- src/TapkuLibrary/TKCalendarMonthView.h
+++ src/TapkuLibrary/TKCalendarMonthView.h
@@ -28,120 +28,58 @@
  OTHER DEALINGS IN THE SOFTWARE.
  
  */
-
-#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
+#import "TKCalendarMonthViewHeader.h"
 
 @class TKCalendarMonthTiles;
-@protocol TKCalendarMonthViewDelegate, TKCalendarMonthViewDataSource;
-
-#pragma mark - TKCalendarMonthView
-/** `TKCalendarMonthView` imitates the month grid in the Calendar app on iPhone. */
-@interface TKCalendarMonthView : UIView
-
-/** Initialize a Calendar Month Grid.
- @param timeZone The time zone of the grid,
- @return A `TKCalendarMonthView` object or nil.
- */
-- (id) initWithTimeZone:(NSTimeZone*)timeZone; // or Monday
+@class TKCalendarMonthViewHeader;
+@protocol TKCalendarMonthViewDelegate, TKCalendarMonthViewDataSource, TKCalendarMonthViewSizeDelegate;
 
 
-/** Initialize a Calendar Month Grid.
- @param sunday Flag to setup the grid with Monday or Sunday as the leftmost day.
- @param timeZone The time zone of the grid,
- @return A `TKCalendarMonthView` object or nil.
- */
-- (id) initWithSundayAsFirst:(BOOL)sunday timeZone:(NSTimeZone*)timeZone;
+@interface TKCalendarMonthView : UIView {
+	TKCalendarMonthTiles *currentTile,*oldTile;
+	UIImageView *shadow;
+	UIScrollView *tileBox;
+	BOOL sunday;
 
-/** Initialize a Calendar Month Grid.
- @param sunday Flag to setup the grid with Monday or Sunday as the leftmost day.
- @return A `TKCalendarMonthView` object or nil.
- */
-- (id) initWithSundayAsFirst:(BOOL)sunday;
+	id <TKCalendarMonthViewDelegate> __weak delegate;
+	id <TKCalendarMonthViewDataSource> __weak dataSource;
+	id <TKCalendarMonthViewSizeDelegate> __weak sizeDelegate;
+}
 
-/** The delegate must adopt the `TKCalendarMonthViewDelegate` protocol. The delegate is not retained. */
-@property (nonatomic,assign) id <TKCalendarMonthViewDelegate> delegate;
+@property (nonatomic)   TKCalendarMonthViewHeader *header;
+@property (nonatomic,weak) id <TKCalendarMonthViewDelegate> delegate;
+@property (nonatomic,weak) id <TKCalendarMonthViewDataSource> dataSource;
+@property (nonatomic,weak) id <TKCalendarMonthViewSizeDelegate> sizeDelegate;
 
-/** The data source must adopt the `TKCalendarMonthViewDataSource` protocol. The data source is not retained. */
-@property (nonatomic,assign) id <TKCalendarMonthViewDataSource> dataSource;
+- (id) initWithSundayAsFirst:(BOOL)sunday; // it sunday regardless right now...
+- (id) initWithFrame:(CGRect) aFrame sundayAsFirst:(BOOL) s;
 
-/** The time zone for calendar grid. */
-@property (nonatomic,strong) NSTimeZone *timeZone;
-
-
-/** The current date highlighted on the month grid.
- @return An `NSDate` object set to the month, year and day of the current selection.
- */
 - (NSDate*) dateSelected;
-
-
-/** The current month date being displayed. 
- @return An `NSDate` object set to the month and year of the current month grid.
- */
 - (NSDate*) monthDate;
+- (void) selectDate:(NSDate*)date;
+- (void) reload;
 
-/** Selects a specific date in the month grid. 
- @param date The date that will be highlighed.
- */
-- (BOOL) selectDate:(NSDate*)date;
+- (CGFloat) maximumHeight;
 
-/** Reloads the current month grid. */
-- (void) reloadData;
-
-/** Animates the calendar to the next or previous month.
- @param isNext YES for next month, NO for previous month.
- */
-- (void) animateToNextOrPreviousMonth:(BOOL)next;
-
-
++ (int) rowsForMonth:(NSDate *) date;
 @end
 
-#pragma mark - TKCalendarMonthViewDelegate
-/** The delegate of a `TKCalendarMonthView` object must adopt the `TKCalendarMonthViewDelegate` protocol. */ 
+
 @protocol TKCalendarMonthViewDelegate <NSObject>
+
 @optional
+- (void) calendarMonthView:(UIView *)monthView didSelectDate:(NSDate*)d;
+- (void) calendarMonthView:(UIView *)monthView monthDidChange:(NSDate*)d;
 
-/** The highlighed date changed.
- @param monthView The calendar month view.
- @param date The highlighted date.
- */ 
-- (void) calendarMonthView:(TKCalendarMonthView*)monthView didSelectDate:(NSDate*)date;
-
-
-/** The calendar should change the current month to grid shown.
- @param monthView The calendar month view.
- @param month The month date.
- @param animated Animation flag
- @return YES if the month should change. NO otherwise
- */ 
-- (BOOL) calendarMonthView:(TKCalendarMonthView*)monthView monthShouldChange:(NSDate*)month animated:(BOOL)animated;
-
-/** The calendar will change the current month to grid shown.
- @param monthView The calendar month view.
- @param month The month date.
- @param animated Animation flag
- */ 
-- (void) calendarMonthView:(TKCalendarMonthView*)monthView monthWillChange:(NSDate*)month animated:(BOOL)animated;
-
-/** The calendar did change the current month to grid shown.
- @param monthView The calendar month view.
- @param month The month date.
- @param animated Animation flag
- */ 
-- (void) calendarMonthView:(TKCalendarMonthView*)monthView monthDidChange:(NSDate*)month animated:(BOOL)animated;
 @end
 
-#pragma mark - TKCalendarMonthViewDataSource
-/** The data source of a `TKCalendarMonthView` object must adopt the `TKCalendarMonthViewDataSource` protocol. */ 
 @protocol TKCalendarMonthViewDataSource <NSObject>
 
-/** A data source that will correspond to marks for the calendar month grid for a particular month.
- @param monthView The calendar month grid.
- @param startDate The first date shown by the calendar month grid.
- @param lastDate The last date shown by the calendar month grid.
- @return Returns an array of NSNumber objects corresponding the number of days specified in the start and last day parameters. Each NSNumber variable will give a BOOL value that will be used to display a dot under the day.
- */
-- (NSArray*) calendarMonthView:(TKCalendarMonthView*)monthView marksFromDate:(NSDate*)startDate toDate:(NSDate*)lastDate;
+- (NSArray*) calendarMonthView:(UIView *)monthView marksFromDate:(NSDate*)startDate toDate:(NSDate*)lastDate;
 
+@end
+
+@protocol TKCalendarMonthViewSizeDelegate <NSObject>
+- (void) calendarMonthView:(UIView *) monthView willAnimateToFrame:(CGRect) rect animationDuration:(NSTimeInterval) interval;
 @end
